97e5cfd34374af1dd194432f223d4976564436be
Cosmin Matei <matei.cosmin88@yahoo.co.uk>
2023-11-10 21:12:04 +0000

    a

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.glowing-btn').addEventListener('click', function() {
+        window.open('https://www.rcl.ac.uk/courses/subjects/engineering-and-computing/', '_blank');
+    });
+});
